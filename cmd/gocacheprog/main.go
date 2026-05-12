304d777e24bf131dd20a8206cb3d6acc677fe180
Viacheslav Poturaev <vearutop@gmail.com>
2026-05-12 13:22:27 +0200

    Refactor runServer

--- cmd/gocacheprog/main.go
+++ cmd/gocacheprog/main.go
@@ -5,13 +5,8 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
-	nethttp "net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"strings"
-	"syscall"
 	"time"
 
 	"github.com/vearutop/gocacheprogd/internal/cache"
@@ -177,66 +172,5 @@ func runServer(listen string, dir string, authToken string, maxDiskBytes int64, 
 	}
 	defer store.Close()
 
-	h := http.NewHandlerWithPreloadLimit(store, authToken, preloadLimit)
-
-	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-			store.PrintStats()
-		}
-	}()
-
-	network, addr := listenNetworkAndAddr(listen)
-	if network == "unix" {
-		if err := os.RemoveAll(addr); err != nil {
-			return fmt.Errorf("remove old unix socket %s: %w", addr, err)
-		}
-		if err := os.MkdirAll(filepath.Dir(addr), 0o755); err != nil {
-			return fmt.Errorf("create unix socket dir: %w", err)
-		}
-	}
-
-	ln, err := net.Listen(network, addr)
-	if err != nil {
-		return fmt.Errorf("listen %s %s: %w", network, addr, err)
-	}
-	if network == "unix" {
-		defer os.Remove(addr)
-	}
-	defer ln.Close()
-
-	server := &nethttp.Server{Handler: h}
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-	defer signal.Stop(stop)
-
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- server.Serve(ln)
-	}()
-
-	log.Printf("Listening on %s://%s ...", network, addr)
-
-	select {
-	case sig := <-stop:
-		log.Printf("Shutting down on %s ...", sig)
-		if err := server.Close(); err != nil {
-			log.Printf("server close: %s", err.Error())
-		}
-	case err := <-errCh:
-		if err != nil && err != nethttp.ErrServerClosed {
-			return fmt.Errorf("serve %s %s: %w", network, addr, err)
-		}
-	}
-
-	return nil
-}
-
-func listenNetworkAndAddr(listen string) (network string, addr string) {
-	if strings.HasPrefix(listen, "unix://") {
-		return "unix", strings.TrimPrefix(listen, "unix://")
-	}
-
-	return "tcp", listen
+	return local.RunServer(listen, store, authToken, preloadLimit)
 }
